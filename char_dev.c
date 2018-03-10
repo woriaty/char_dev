3b80cdfbd46b1b40f8883aa06302c5cf6e5c8fe9
woriaty <1546168053@qq.com>
2018-03-10 22:37:52 +0800

    add dev framwork

--- char_dev.c
+++ char_dev.c
@@ -6,6 +6,32 @@
 #include <linux/slab.h>
 
 
+static ssize_t char_dev_read(struct file *,const char __user *,size_t,loff_t *)
+{
+	return 0;
+}
+
+static ssize_t char_dev_write(struct file *,const char __user *,size_t,loff_t *)
+{
+	return 0;
+}
+
+static int char_dev_open(struct inode *, struct file *)
+{
+	return 0;
+}
+
+static int char_dev_release(struct inode *, struct file *)
+{
+	return 0;
+}
+
+struct file_operations char_dev_ops{
+	.read    = char_dev_read;
+	.write   = char_dev_write;
+	.open    = char_dev_open;
+	.release = char_dev_release;
+};
 
 static int __init char_dev_init(void)
 {
